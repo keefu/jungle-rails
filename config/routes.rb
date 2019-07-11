46a490ddea4215463f5a58e11a1fa36b2caf498a
keefu <cyuplia@gmail.com>
2019-07-11 23:39:49 +0000

    Complete user authentification

--- config/routes.rb
+++ config/routes.rb
@@ -25,8 +25,10 @@ Rails.application.routes.draw do
 
   get '/login', to: 'sessions#new'
   post '/sessions', to: 'sessions#create'
+  delete '/sessions', to: 'sessions#destroy'
 
-  
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
