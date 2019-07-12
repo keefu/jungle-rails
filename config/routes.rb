c1d212d950f0c67645fbeec6890bec8868d69aa0
keefu <cyuplia@gmail.com>
2019-07-12 20:29:38 +0000

    Complete order details

--- config/routes.rb
+++ config/routes.rb
@@ -27,7 +27,7 @@ Rails.application.routes.draw do
   post '/sessions', to: 'sessions#create'
   delete '/sessions', to: 'sessions#destroy'
 
-
+  
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
